308f87136b7768611b1ba101ab643a4189316c54
abhiksark <abhiksark@gmail.com>
2017-07-19 14:08:26 +0530

    Demo Ready

--- inputData.py
+++ inputData.py
@@ -15,44 +15,39 @@ class inputPictures():
         self.face_cascade =cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
         self.trainData=[]
         self.testData =[]
+        self.testDict = dict()
         self.responseData=[]
         self.trainFolder = './data/' 
         self.reverseName=dict()
         #self.reverseKey=0
-    
-    def num2name(self,num):
-        if num==0:
-            name = 'Modi'
-        elif num ==1:
-            name = 'Raga'
-        elif num ==2:
-            name = 'Kohli'
-        elif num==3:
-            name = 'Salman'
-        elif num ==4:
-            name = 'Priyanka'
-        elif num ==5:
-            name = 'Musk'
-        elif num==6:
-            name = 'Gal'
-        elif num==7:
-            name = 'Sandberg'
-        elif num==8:
-            name = 'Saina'
-        return name
-    
     def takeInput(self,name,reverseKey):
         self.reverseName[reverseKey]=name
         name = self.trainFolder+str(name)+'/'
         image = [ f for f in listdir(name) if isfile(join(name,f)) ]
         self.ReadImages(image,name,int(reverseKey))
-    
+    def inputTestData(self,name):
+        name = self.trainFolder+str(name)+'/'
+        images = [ f for f in listdir(name) if isfile(join(name,f))]
+        for image in images:
+            img = cv2.imread(join(name,image))
+            try:
+                imgray = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+                face = self.face_cascade.detectMultiScale(imgray,minSize = (50, 50),scaleFactor = 1.25) 
+                if len(face)>0:
+                    feature = HOG(cv2.resize(imgray[face[0][1]:face[0][1]+face[0][3],face[0][0]:face[0][0]+face[0][2]],
+                                         (100,100)))
+                    self.testData.append(feature.T)
+                    self.testDict[image]=feature.T
+            except Exception as e:
+                print(e)
+                pass
+        
     def ReadImages(self,ListName,FolderName,Label):   
         for image in ListName:
             img = cv2.imread(join(FolderName,image))
             try:
                 imgray = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
-                face = self.face_cascade.detectMultiScale(imgray) 
+                face = self.face_cascade.detectMultiScale(imgray,minSize = (50, 50),scaleFactor = 1.25) 
                 if len(face)>0:
                     feature = HOG(cv2.resize(imgray[face[0][1]:face[0][1]+face[0][3],face[0][0]:face[0][0]+face[0][2]],
                                          (100,100)))
@@ -63,6 +58,7 @@ class inputPictures():
                 pass
     def returnTrainData(self):
         return self.trainData,self.responseData
-    
+    def returnTestData(self):
+        return self.testData,self.testDict
     def returnReverseName(self):
         return self.reverseName
--- prototypeModel.py
+++ prototypeModel.py
@@ -17,36 +17,34 @@ from os import listdir
 from os.path import isfile,join
 #from sklearn.grid_search import GridSearchCV
 import pandas as pd
-df = pd.read_csv("Faces.csv")
-Y = df[['Face']]
-print(Y)
-
-X=df.iloc[:,1:].values
-y=df.iloc[:,0:1].values
-#df = pd.DataFrame([X,y])
-#df.to_csv("faces.csv")
-#imagesObj = inputPictures()
+#df = pd.read_csv("Faces.csv")
+#Y = df[['Face']]
+#print(Y)
+#
+#X=df.iloc[:,1:].values
+#y=df.iloc[:,0:1].values
+imagesObj = inputPictures()
 #imagesObj.takeInput('Modi',0)
-#imagesObj.takeInput('Raga',1)
-##imagesObj.takeInput('Gal',2)
-#imagesObj.takeInput('Kohli',2)
-#imagesObj.takeInput('RanveerS',3)
-#imagesObj.takeInput('Salman',4)
-#imagesObj.takeInput('Musk',5)
-##imagesObj.takeInput('Sandberg',7)
+imagesObj.takeInput('Raga',0)
+#imagesObj.takeInput('Gal',2)
+imagesObj.takeInput('Kohli',1)
+imagesObj.takeInput('RanveerS',2)
+imagesObj.takeInput('Salman',3)
+imagesObj.takeInput('Musk',4)
+#imagesObj.takeInput('Sandberg',7)
 #imagesObj.takeInput('Obama',6)
 #imagesObj.takeInput('Priyanka',7)
-predDict={0:'Modi',1:'Raga',2:'Kohli',3:'RanveerS',4:'Salman',5:'Musk',6:'Obama',7:'Priyanka'}
+#predDict={0:'Modi',1:'Raga',2:'Kohli',3:'RanveerS',4:'Salman',5:'Musk',6:'Obama',7:'Priyanka'}
 #imagesObj.takeInput('Saina',8)
-#predDict = imagesObj.returnReverseName()
-#X,y=imagesObj.returnTrainData()
-
+predDict = imagesObj.returnReverseName()
+X,y=imagesObj.returnTrainData()
+#
 #y=np.array(y)
-#y=np.resize(y,(1326,1))
+#y=np.resize(y,(4257,1))
 #newX=np.asarray(X)
 #new=np.concatenate((y,newX), axis=1)
 #df = pd.DataFrame(new)
-#df.to_csv("Faces.csv")
+#df.to_csv("Faces.csv",index=False)
 
 from sklearn.preprocessing import scale
 X = scale( X, axis=0, with_mean=True, with_std=True, copy=True )
@@ -68,7 +66,7 @@ TestImages = [ f for f in listdir(Test) if isfile(join(Test,f)) ]
 for image in TestImages:
     img = cv2.imread(join(Test,image))
     imgray=cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
-    face = face_cascade.detectMultiScale(imgray)
+    face = face_cascade.detectMultiScale(imgray,minSize = (50, 50),scaleFactor = 1.25)
     if len(face)>0:
             feature = HOG(cv2.resize(imgray[face[0][1]:face[0][1]+face[0][3],face[0][0]:face[0][0]+face[0][2]],
                                  (100,100)))
@@ -77,4 +75,35 @@ for image in TestImages:
     plt.figure()
     plt.imshow(cv2.cvtColor(img,cv2.COLOR_BGR2RGB))
     plt.title(predDict[int(pred)])
+    try:
+            image = cv2.imread(join(Test,image))
+            imgray = cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
+            face_cascade =cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+            face = face_cascade.detectMultiScale(imgray,minSize = (50, 50),scaleFactor = 1.25) 
+            if len(face)>0:
+                feature,hog_image= HOG(cv2.resize(imgray[face[0][1]:face[0][1]+face[0][3],face[0][0]:face[0][0]+face[0][2]],(100,100)),cells_per_block=(1, 1), visualise=True)                        
+                cropped_image=imgray[face[0][1]:face[0][1]+face[0][3],face[0][0]:face[0][0]+face[0][2]]
+                fig, (ax1, ax2) = plt.subplots(1, 2)
+                
+                ax1.axis('off')
+                ax1.imshow(cropped_image, cmap=plt.cm.gray)
+                ax1.set_title('Input image')
+                ax1.set_adjustable('box-forced')
+                
+                # Rescale histogram for better display
+                #hog_image_rescaled = exposure.rescale_intensity(hog_image, in_range=(0, 0.02))
+                
+                ax2.axis('off')
+                ax2.imshow(hog_image, cmap=plt.cm.gray)
+                ax2.set_title('Histogram of Oriented Gradients')
+                ax1.set_adjustable('box-forced')
+                plt.show()
+            else:
+                continue
+    except Exception as e:
+            print(e)
+            pass
+        
+        
+
 
